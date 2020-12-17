951eac196bb462578de6aa52d640ebdb51911e62
Vitor Pires <vitor.pires@thoughtworks.com>
2020-12-17 19:44:09 -0300

    first commit

---
+++ TestFrame/TestFrame.h
@@ -0,0 +1,18 @@
+//
+//  TestFrame.h
+//  TestFrame
+//
+//  Created by LAVitor Pires on 17/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFrame.
+FOUNDATION_EXPORT double TestFrameVersionNumber;
+
+//! Project version string for TestFrame.
+FOUNDATION_EXPORT const unsigned char TestFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrame/PublicHeader.h>
+
+
